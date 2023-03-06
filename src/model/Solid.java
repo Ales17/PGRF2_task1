a4a997cb66eb4278ecf23ef5d34924e9b16d3a6a
Ales Bacul <alesbacul@gmail.com>
2023-03-06 21:05:42 +0100

    Encapsulation

--- src/model/Solid.java
+++ src/model/Solid.java
@@ -4,18 +4,16 @@ import transforms.Mat4;
 import transforms.Mat4Identity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Solid {
 
-    private ArrayList<Vertex> vertexBuffer;
-    private ArrayList<Integer> indexBuffer;
-    private ArrayList<Part> partBuffer;
+    private List<Vertex> vertexBuffer = new ArrayList<>();
+    private List<Integer> indexBuffer = new ArrayList<>();
+    private List<Part> partBuffer = new ArrayList<>()
 
     Mat4 model = new Mat4Identity();
-    public Solid() { // ArrayList !!!
-         vertexBuffer = new ArrayList<>();
-         indexBuffer = new ArrayList<>();
-         partBuffer = new ArrayList<>();
+    public Solid() {
     }
 
     public Mat4 getModelMatrix() {
@@ -26,30 +24,15 @@ public abstract class Solid {
         this.model = model;
     }
 
-    public void setVertexBuffer(ArrayList<Vertex> vertexBuffer) {
-        this.vertexBuffer = vertexBuffer;
-    }
-
-    public void setIndexBuffer(ArrayList<Integer> indexBuffer) {
-        this.indexBuffer = indexBuffer;
-    }
-
-    public void setPartBuffer(ArrayList<Part> partBuffer) {
-        this.partBuffer = partBuffer;
-    }
-
-
-
-
-    public ArrayList<Vertex> getVertexBuffer() {
+    public List<Vertex> getVertexBuffer() {
         return vertexBuffer;
     }
 
-    public ArrayList<Integer> getIndexBuffer() {
+    public List<Integer> getIndexBuffer() {
         return indexBuffer;
     }
 
-    public ArrayList<Part> getPartBuffer() {
+    public List<Part> getPartBuffer() {
         return partBuffer;
     }
 
